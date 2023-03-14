46c9e92b402631b83e899255d8a7e018b09d9f28
addisonjadwin <addisonjadwin@hotmail.com>
2023-03-13 19:03:07 -0700

    capitalizing T

--- minbert-default-final-project/classifier_MLM.py
+++ minbert-default-final-project/classifier_MLM.py
@@ -8,8 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import classification_report, f1_score, recall_score, accuracy_score
 
 # change it with respect to the original model
-import tokenizer
-from tokenizer import BertTokenizer, SpecialTokensMixin
+from tokenizer import BertTokenizer, SpecialTokensMixin, PreTrainedTokenizer
 
 from bert import BertModel
 from optimizer import AdamW
